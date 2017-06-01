ecb770d764d3de47598a3c4bdc9974bc20c8f973
Stephen Kalmakis <stephen@hioscar.com>
2017-06-01 09:09:49 -0400

    Pro tips

--- app/Travel.jsx
+++ app/Travel.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Page from './Page';
 import Section from './Section';
 
+import css from './Travel.scss';
+
 import img from './images/travel-hero.jpg';
 
 export default React.createClass({
@@ -13,9 +15,16 @@ export default React.createClass({
           <h3>From NYC</h3>
           <p>Take MetroNorth from Grand Central Terminal to Tarrytown on the Hudson line toward Croton-Harmon.</p>
           <a href="http://tripplanner.mta.info/mytrip/ui_web/customplanner/TripPlanner.aspx">See schedule</a>
+          <p className={css.subtext}>
+            Pro tip: Sit on the west side of the train for stunning views of the Hudson!
+            (The left side, when leaving Manhattan.)
+          </p>
 
           <h3>From outside New York City</h3>
           <p><a href="https://goo.gl/maps/onfiKVpHRqM2" target="_blank">Use Google</a>. They know better than we do!</p>
+          <p className={css.subtext}>
+            For those flying, Westchester County Airport is the closest.
+          </p>
         </Section>
 
         <Section title="Getting to Tappan Hill Mansion">
