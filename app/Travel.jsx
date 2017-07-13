c6f864ecb6b8367c0a219e3447a91fbe3d84263d
Stephen Kalmakis <stephen@hioscar.com>
2017-07-12 23:11:00 -0400

    Add note about cabs and ubers

--- app/Travel.jsx
+++ app/Travel.jsx
@@ -7,6 +7,9 @@ import css from './Travel.scss';
 
 import img from './images/travel-hero.jpg';
 
+const NONBREAKING_SPACE = '\u00a0';
+
+
 export default React.createClass({
   render() {
     return (
@@ -15,6 +18,8 @@ export default React.createClass({
           <h3>From NYC</h3>
           <p>Take MetroNorth from Grand Central Terminal to Tarrytown on the Hudson line toward Croton-Harmon.</p>
           <a href="http://tripplanner.mta.info/mytrip/ui_web/customplanner/TripPlanner.aspx">See schedule</a>
+          <p>{NONBREAKING_SPACE}</p>
+          <p>Cabs are available from the train station, and Ubers are plentiful.</p>
           <p className={css.subtext}>
             Pro tip: Sit on the west side of the train for stunning views of the Hudson!
             (The left side, when leaving Manhattan.)
